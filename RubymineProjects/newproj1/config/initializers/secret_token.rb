17830f5da2c019feff2a4ec171b6ad50ff2c74c9
JP <jdasher5@gmail.com>
2012-06-26 13:14:24 -0400

    Initial commit

---
+++ RubymineProjects/newproj1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newproj1::Application.config.secret_token = '2b00d28da4d24b8eecd0253b9858de6b91874d9ba3b34c09e25ddcf22ed037a07d3214eb2ad9c0cf605ccd9d9c4456a4318c71e265898d59bb8517f4e3f91240'
